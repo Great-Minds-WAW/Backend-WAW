0ac009c689b5d902640876b7d1ebae800a51c2e3
kevin <67239585+kevinstif@users.noreply.github.com>
2022-09-27 08:59:03 -0500

    Revert "eliminacion momentanea"
    This reverts commit 60203a7e6df2cd1052052c07a6223c02432c5888.

---
+++ src/test/java/com/example/WAW/WawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
